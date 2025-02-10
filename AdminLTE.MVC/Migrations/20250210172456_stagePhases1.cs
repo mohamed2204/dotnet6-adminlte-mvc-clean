8ed521912b0f47ecb088e52d567631eee9fe0288
mohamed2204 <moi2204@gmail.com>
2025-02-10 23:12:11 +0100

    StagePhase

---
+++ AdminLTE.MVC/Migrations/20250210172456_stagePhases1.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminLTE.MVC.Migrations
+{
+    public partial class stagePhases1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Promotion",
+                table: "Stages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Stages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Phases",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PhaseId",
+                table: "Phases",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StageId",
+                table: "Phases",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Phases_PhaseId",
+                table: "Phases",
+                column: "PhaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Phases_StageId",
+                table: "Phases",
+                column: "StageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phases_Phases_PhaseId",
+                table: "Phases",
+                column: "PhaseId",
+                principalTable: "Phases",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phases_Stages_StageId",
+                table: "Phases",
+                column: "StageId",
+                principalTable: "Stages",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phases_Phases_PhaseId",
+                table: "Phases");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phases_Stages_StageId",
+                table: "Phases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Phases_PhaseId",
+                table: "Phases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Phases_StageId",
+                table: "Phases");
+
+            migrationBuilder.DropColumn(
+                name: "PhaseId",
+                table: "Phases");
+
+            migrationBuilder.DropColumn(
+                name: "StageId",
+                table: "Phases");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Promotion",
+                table: "Stages",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Stages",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Phases",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
--- AdminLTE.MVC/Models/Matiere.cs
+++ AdminLTE.MVC/Models/Matiere.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using AdminLTE.MVC.Migrations;
+using Microsoft.Extensions.Hosting;
 
 namespace AdminLTE.MVC.Models
 {
@@ -15,10 +18,12 @@ namespace AdminLTE.MVC.Models
     public partial class Stage
     {
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
-
+        [Required]
         public string Promotion { get; set; }
+
+        public List<StagePhase> StagePhases { get; }
 
 
     }
@@ -26,16 +31,23 @@ namespace AdminLTE.MVC.Models
     public partial class Phase
     {
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
+
+        public List<StagePhase> StagePhases { get; }
 
     }
 
-    //public partial class StagePhases
-    //{
-    //    public int StageId { get; set; }
+    public partial class StagePhase
+    {
+     
+        public int StageId { get; set; }
 
-    //    public int PhaseId { get; set; }
+        public int PhaseId { get; set; }
 
-    //}
+        public Stage Stage { get; set; } = null!;
+
+        public Phase Phase { get; set; } = null!;
+
+    }
 }
