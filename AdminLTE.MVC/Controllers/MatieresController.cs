b583203d2aa8d723c561147ad99e06107f9c446a
mjena <moi2204@gmail.com>
2025-02-11 10:34:06 +0100

    aa

---
+++ AdminLTE.MVC/Controllers/MatieresController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AdminLTE.MVC.Data;
+using AdminLTE.MVC.Models;
+
+namespace AdminLTE.MVC.Controllers
+{
+    public class MatieresController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MatieresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Matieres
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Matieres.ToListAsync());
+        }
+
+        // GET: Matieres/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Matieres == null)
+            {
+                return NotFound();
+            }
+
+            var matiere = await _context.Matieres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (matiere == null)
+            {
+                return NotFound();
+            }
+
+            return View(matiere);
+        }
+
+        // GET: Matieres/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Matieres/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Matiere matiere)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(matiere);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(matiere);
+        }
+
+        // GET: Matieres/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Matieres == null)
+            {
+                return NotFound();
+            }
+
+            var matiere = await _context.Matieres.FindAsync(id);
+            if (matiere == null)
+            {
+                return NotFound();
+            }
+            return View(matiere);
+        }
+
+        // POST: Matieres/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name")] Matiere matiere)
+        {
+            if (id != matiere.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(matiere);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MatiereExists(matiere.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(matiere);
+        }
+
+        // GET: Matieres/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Matieres == null)
+            {
+                return NotFound();
+            }
+
+            var matiere = await _context.Matieres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (matiere == null)
+            {
+                return NotFound();
+            }
+
+            return View(matiere);
+        }
+
+        // POST: Matieres/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Matieres == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Matieres'  is null.");
+            }
+            var matiere = await _context.Matieres.FindAsync(id);
+            if (matiere != null)
+            {
+                _context.Matieres.Remove(matiere);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MatiereExists(long id)
+        {
+          return _context.Matieres.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ AdminLTE.MVC/Migrations/20250210231037_stagePhase0010.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminLTE.MVC.Migrations
+{
+    public partial class stagePhase0010 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phases_Phases_PhaseId",
+                table: "Phases");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phases_Stages_StageId",
+                table: "Phases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Phases_PhaseId",
+                table: "Phases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Phases_StageId",
+                table: "Phases");
+
+            migrationBuilder.DropColumn(
+                name: "PhaseId",
+                table: "Phases");
+
+            migrationBuilder.DropColumn(
+                name: "StageId",
+                table: "Phases");
+
+            migrationBuilder.CreateTable(
+                name: "StagePhases",
+                columns: table => new
+                {
+                    StageId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PhaseId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AddedOn = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StagePhases", x => new { x.StageId, x.PhaseId });
+                    table.ForeignKey(
+                        name: "FK_StagePhases_Phases_PhaseId",
+                        column: x => x.PhaseId,
+                        principalTable: "Phases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StagePhases_Stages_StageId",
+                        column: x => x.StageId,
+                        principalTable: "Stages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StagePhases_PhaseId",
+                table: "StagePhases",
+                column: "PhaseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StagePhases");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PhaseId",
+                table: "Phases",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StageId",
+                table: "Phases",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Phases_PhaseId",
+                table: "Phases",
+                column: "PhaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Phases_StageId",
+                table: "Phases",
+                column: "StageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phases_Phases_PhaseId",
+                table: "Phases",
+                column: "PhaseId",
+                principalTable: "Phases",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phases_Stages_StageId",
+                table: "Phases",
+                column: "StageId",
+                principalTable: "Stages",
+                principalColumn: "Id");
+        }
+    }
+}
